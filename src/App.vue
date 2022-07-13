1f41567374401d96f5503c2542bd676d3e057028
Andy <andy@Andys-MacBook-Pro.local>
2022-07-13 01:20:55 -0400

    code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="nav-bar"></div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="cart">Cart({{ cart.length }})</div>
+  <product-display :premium="premium" @add-to-cart="updateCart"></product-display>
 </template>
 
+<script>
+import ProductDisplay from './components/ProductDisplay.vue'
+
+export default {
+  components: {
+    ProductDisplay
+  },
+  data() {
+    return {
+      cart: [],
+      premium: true
+    }
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    }
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/ReviewList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="review-container">
+  <h3>Reviews:</h3>
+    <ul>
+      <li v-for="(review, index) in reviews" :key="index">
+        {{ review.name }} gave this {{ review.rating }} stars
+        <br/>
+        "{{ review.review }}"
+        <br/>
+        Recommended: {{ review.recommend }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    reviews: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
