f62c3c2542d9618772a3982d17f13f2f437125a2
Andy <andy@Andys-MacBook-Pro.local>
2022-07-31 18:10:02 -0400

    refactor App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@ function updateCart(id) {
 </script>
 
 <template>
-  <div class="nav-bar">
+  <div class="h-16 mb-6 bg-gradient-to-r from-[#16c0b0] to-[#84cf6a]">
   </div>
-  <div class="cart">
+  <div class="my-6 mx-24 p-7 border border-solid border-[#d8d8d8] bg-white float-right">
     Cart({{ cart.length }})
   </div>
   <product-display :premium="premium" @add-to-cart="updateCart"></product-display>
@@ -21,7 +21,7 @@ function updateCart(id) {
 
 <style scoped>
 
-.nav-bar {
+/* .nav-bar {
   height: 60px;
   margin-bottom: 25px;
   background: linear-gradient(90deg, #16c0b0, #84cf6a);
@@ -33,5 +33,5 @@ function updateCart(id) {
   border: 1px solid #d8d8d8;
   background-color: white;
   float: right;
-}
+} */
 </style>
